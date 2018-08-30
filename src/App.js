aa4f86475ddb5d3c1d9af4dfcaf2dde09f9ce60f
Jeffery Schiers <schiersj@gmail.com>
2018-08-30 14:01:51 -0500

    Setup routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
-import Movies from "./components/movies";
+import { Route, Redirect, Switch } from "react-router-dom";
+
 import "./App.css";
+
+import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
 
 class App extends Component {
   render() {
     return (
       <main className="container">
         <Movies />
+        <Switch>
+          <Route path="/movies" compoent={Movies} />
+          <Route path="/customers" compoent={Customers} />
+          <Route path="/rentals" compoent={Rentals} />
+          <Route path="/not-found" compoent={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
       </main>
     );
   }
