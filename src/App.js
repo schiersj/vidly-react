4e6511378df34519246a2f849c0ec53f2297b080
Jeffery Schiers <schiersj@gmail.com>
2018-08-30 14:22:44 -0500

    Link navigation for movies form in movies table

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React, { Component } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
 
-import "./App.css";
-
 import Movies from "./components/movies";
+import MovieForm from "./components/movieForm";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Movies />
-        <Switch>
-          <Route path="/movies" compoent={Movies} />
-          <Route path="/customers" compoent={Customers} />
-          <Route path="/rentals" compoent={Rentals} />
-          <Route path="/not-found" compoent={NotFound} />
-          <Redirect from="/" exact to="/movies" />
-          <Redirect to="/not-found" />
-        </Switch>
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
