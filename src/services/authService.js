938830f9312b7054d3e9f6c86d03465e4b996271
Jeffery Schiers <schiersj@gmail.com>
2018-08-31 14:20:59 -0500

    refactor token handling to authService

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,10 +1,35 @@
+import jwtDecode from "jwt-decode";
 import http from "./httpService";
 import { apiUrl } from "../config.json";
 
 const apiEndpoint = apiUrl + "/auth";
+const tokenKey = "token";
 
-export function login(email, password) {
-  return http.post(apiEndpoint, { email, password });
+export async function login(email, password) {
+  const { data: jwt } = await http.post(apiEndpoint, { email, password });
+  localStorage.setItem(tokenKey, jwt);
 }
 
-export function logout(user) {}
+export function loginWithJwt(jwt) {
+  localStorage.setItem(tokenKey, jwt);
+}
+
+export function logout(user) {
+  localStorage.removeItem(tokenKey);
+}
+
+export function getCurrentUser() {
+  try {
+    const jwt = localStorage.getItem(tokenKey);
+    return jwtDecode(jwt);
+  } catch (ex) {
+    return null;
+  }
+}
+
+export default {
+  login,
+  loginWithJwt,
+  logout,
+  getCurrentUser
+};
