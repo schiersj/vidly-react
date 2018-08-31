fad82c1a6751e2a32337c1ac44afd6ff8ad15d72
Jeffery Schiers <schiersj@gmail.com>
2018-08-31 14:44:34 -0500

    send user to movies component and hide new button if user not logged in

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -104,6 +104,7 @@ class Movies extends Component {
       searchQuery,
       sortColumn
     } = this.state;
+    const { user } = this.props;
 
     if (count === 0) return <p>There are no movies in the database.</p>;
 
@@ -119,13 +120,15 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
-          <Link
-            to="movies/new"
-            className="btn btn-primary"
-            style={{ marginBottom: 20 }}
-          >
-            New Movie
-          </Link>
+          {user && (
+            <Link
+              to="movies/new"
+              className="btn btn-primary"
+              style={{ marginBottom: 20 }}
+            >
+              New Movie
+            </Link>
+          )}
           <p>Showing {totalCount} movies in the database.</p>
           <SearchBox value={searchQuery} onChange={this.handleSearch} />
           <MoviesTable
