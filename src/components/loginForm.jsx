90c491f897383a88d4898fad4bba9d8adee44338
Jeffery Schiers <schiersj@gmail.com>
2018-08-31 13:28:07 -0500

    initial setup of authService and login function

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
+import { login } from "../services/authService";
 
 class LoginForm extends Form {
   state = {
@@ -17,7 +18,18 @@ class LoginForm extends Form {
       .label("Password")
   };
 
-  doSubmit = () => {};
+  doSubmit = async () => {
+    try {
+      const { data } = this.state;
+      await login(data.username, data.password);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+    }
+  };
 
   render() {
     return (
---
+++ src/services/authService.js
@@ -0,0 +1,10 @@
+import http from "./httpService";
+import { apiUrl } from "../config.json";
+
+const apiEndpoint = apiUrl + "/auth";
+
+export function login(email, password) {
+  return http.post(apiEndpoint, { email, password });
+}
+
+export function logout(user) {}
