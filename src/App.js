66f10741866b9111554d093354e9aadb5e26f481
Jeffery Schiers <schiersj@gmail.com>
2018-08-31 08:41:47 -0500

    Add RegisterForm

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
 import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
 
 import "./App.css";
 
@@ -18,6 +19,7 @@ class App extends Component {
         <NavBar />
         <main className="container">
           <Switch>
+            <Route path="/register" component={RegisterForm} />
             <Route path="/login" component={LoginForm} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies} />
